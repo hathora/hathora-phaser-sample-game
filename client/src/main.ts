4bff00f22d7b9a1ec41d958a01643d38569b69a1
Kirk Morris <worker.kirk@gmail.com>
2023-07-04 21:41:23 -0700

    Bumps to hathora-phaser 0.0.3.

--- client/src/main.ts
+++ client/src/main.ts
@@ -34,7 +34,7 @@ class TitleScene extends Scene {
     this.add.haRegionSelect(1280 / 2, 128);
 
     // @ts-ignore
-    this.add.haCreateGameButton(1280 / 2, 192);
+    this.add.haCreateGameButton(1280 / 2, 192, 'Create Gaemu', true);
 
     // @ts-ignore
     this.add.haJoinPrivateInput(1280 / 2, 256);
